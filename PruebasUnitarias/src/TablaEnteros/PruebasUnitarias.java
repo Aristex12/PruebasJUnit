eda7fcbd45dfe8a46aecc04b0c04e152291edf8b
Miguel Sanz <118904961+Miguel-Sanz@users.noreply.github.com>
2023-05-10 17:50:17 +0200

    Versión Miguel

---
+++ PruebasUnitarias/src/TablaEnteros/PruebasUnitarias.java
@@ -0,0 +1,41 @@
+package TablaEnteros;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.*;
+
+class PruebasUnitarias {
+	
+	static TablaEnteros objeto;
+	
+	@BeforeAll
+	public static void inicializar() {
+		Integer[] numeros = new Integer[5];
+		objeto = new TablaEnteros(numeros);
+		numeros[0] = 2;
+		numeros[1] = 4;
+		numeros[2] = 6;
+		numeros[3] = 8;
+		numeros[4] = 10;
+	}
+	
+	@Test
+	void testSumaTabla() {
+		assertEquals(30, objeto.sumaTabla());
+	}
+	
+	@Test
+	void testMayorTabla() {
+		assertEquals(10, objeto.mayorTabla());
+	}
+	
+	@Test
+	void testPosicionTabla() {
+		assertEquals(4, objeto.posicionTabla(10));
+	}
+	
+
+
+}
